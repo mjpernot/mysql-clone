513ca32efdc7a4a5b3aedd95730244178b969001
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-04-21 14:49:57 +0100

    Update unit test runs

--- test/unit/mysql_clone/unit_test_run.sh
+++ test/unit/mysql_clone/unit_test_run.sh
@@ -15,4 +15,5 @@ test/unit/mysql_clone/chk_slv_thr.py
 test/unit/mysql_clone/crt_dump_cmd.py
 test/unit/mysql_clone/dump_load_dbs.py
 test/unit/mysql_clone/help_message.py
+test/unit/mysql_clone/run_program.py
 test/unit/mysql_clone/stop_clr_rep.py
