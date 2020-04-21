873e935c5f9b038555025fd8a3cde473fd7979c0
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-04-21 14:53:38 +0100

    Add test/unit/mysql_clone/main.py v2.1.0

---
+++ test/unit/mysql_clone/main.py
@@ -0,0 +1,141 @@
+#!/usr/bin/python
+# Classification (U)
+
+"""Program:  main.py
+
+    Description:  Unit testing of main in mysql_clone.py.
+
+    Usage:
+        test/unit/mysql_clone/main.py
+
+    Arguments:
+
+"""
+
+# Libraries and Global Variables
+
+# Standard
+import sys
+import os
+
+if sys.version_info < (2, 7):
+    import unittest2 as unittest
+else:
+    import unittest
+
+# Third-party
+import mock
+
+# Local
+sys.path.append(os.getcwd())
+import mysql_clone
+import lib.gen_libs as gen_libs
+import version
+
+__version__ = version.__version__
+
+
+class UnitTest(unittest.TestCase):
+
+    """Class:  UnitTest
+
+    Description:  Class which is a representation of a unit testing.
+
+    Methods:
+        setUp -> Initialize testing environment.
+        test_help_true -> Test help if returns true.
+        test_help_false -> Test help if returns false.
+        test_arg_req_true -> Test arg_require if returns true.
+        test_arg_req_false -> Test arg_require if returns false.
+
+    """
+
+    def setUp(self):
+
+        """Function:  setUp
+
+        Description:  Initialization for unit testing.
+
+        Arguments:
+
+        """
+
+        self.args_array = {"-c": "CfgFile", "-d": "CfgDir"}
+
+    @mock.patch("mysql_clone.gen_libs.help_func")
+    @mock.patch("mysql_clone.arg_parser.arg_parse2")
+    def test_help_true(self, mock_arg, mock_help):
+
+        """Function:  test_help_true
+
+        Description:  Test help if returns true.
+
+        Arguments:
+
+        """
+
+        mock_arg.return_value = self.args_array
+        mock_help.return_value = True
+
+        self.assertFalse(mysql_clone.main())
+
+    @mock.patch("mysql_clone.gen_libs.help_func")
+    @mock.patch("mysql_clone.arg_parser")
+    def test_help_false(self, mock_arg, mock_help):
+
+        """Function:  test_help_false
+
+        Description:  Test help if returns false.
+
+        Arguments:
+
+        """
+
+        mock_arg.arg_parse2.return_value = self.args_array
+        mock_help.return_value = False
+        mock_arg.arg_require.return_value = True
+
+        self.assertFalse(mysql_clone.main())
+
+    @mock.patch("mysql_clone.gen_libs.help_func")
+    @mock.patch("mysql_clone.arg_parser")
+    def test_arg_req_true(self, mock_arg, mock_help):
+
+        """Function:  test_arg_req_true
+
+        Description:  Test arg_require if returns true.
+
+        Arguments:
+
+        """
+
+        mock_arg.arg_parse2.return_value = self.args_array
+        mock_help.return_value = False
+        mock_arg.arg_require.return_value = True
+
+        self.assertFalse(mysql_clone.main())
+
+    @mock.patch("mysql_clone.run_program")
+    @mock.patch("mysql_clone.gen_libs.help_func")
+    @mock.patch("mysql_clone.arg_parser")
+    def test_arg_req_false(self, mock_arg, mock_help, mock_run):
+
+        """Function:  test_arg_req_false
+
+        Description:  Test arg_require if returns false.
+
+        Arguments:
+
+        """
+
+        mock_arg.arg_parse2.return_value = self.args_array
+        mock_help.return_value = False
+        mock_arg.arg_require.return_value = False
+        mock_arg.arg_xor_dict.return_value = False
+        mock_run.return_value = True
+
+        self.assertFalse(mysql_clone.main())
+
+
+if __name__ == "__main__":
+    unittest.main()
