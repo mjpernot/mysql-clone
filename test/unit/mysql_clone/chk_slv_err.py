154b8a62106fea1649ab56a0ca189568f6e1bef9
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-04-21 13:18:34 +0100

    Add test/unit/mysql_clone/chk_slv_err.py v2.1.0

---
+++ test/unit/mysql_clone/chk_slv_err.py
@@ -0,0 +1,199 @@
+#!/usr/bin/python
+# Classification (U)
+
+"""Program:  chk_slv_err.py
+
+    Description:  Unit testing of chk_slv_err in mysql_clone.py.
+
+    Usage:
+        test/unit/mysql_clone/chk_slv_err.py
+
+    Arguments:
+
+"""
+
+# Libraries and Global Variables
+
+# Standard
+import sys
+import os
+
+if sys.version_info < (2, 7):
+    import unittest2 as unittest
+else:
+    import unittest
+
+# Third-party
+import mock
+
+# Local
+sys.path.append(os.getcwd())
+import mysql_clone
+import lib.gen_libs as gen_libs
+import version
+
+__version__ = version.__version__
+
+
+class Server(object):
+
+    """Class:  Server
+
+    Description:  Class stub holder for mysql_class.Server class.
+
+    Methods:
+        __init__ -> Class initialization.
+        get_err_stat -> get_err_stat function.
+        get_name -> get_name function.
+
+    """
+
+    def __init__(self):
+
+        """Method:  __init__
+
+        Description:  Class initialization.
+
+        Arguments:
+
+        """
+
+        self.io = "IO"
+        self.sql = "SQL"
+        self.io_msg = "IO_Messages"
+        self.sql_msg = "SQL_Messages"
+        self.io_time = "IO_Time"
+        self.sql_time = "SQL_Time"
+        self.name = "ServerName"
+
+    def get_err_stat(self):
+
+        """Method:  get_err_stat
+
+        Description:  get_err_stat function.
+
+        Arguments:
+
+        """
+
+        return self.io, self.sql, self.io_msg, self.sql_msg, self.io_time, \
+               self.sql_time
+
+    def get_name(self):
+
+        """Method:  get_name
+
+        Description:  get_name function.
+
+        Arguments:
+
+        """
+
+        return self.name
+
+
+class UnitTest(unittest.TestCase):
+
+    """Class:  UnitTest
+
+    Description:  Class which is a representation of a unit testing.
+
+    Methods:
+        setUp -> Initialize testing environment.
+        test_no_error -> Test with no errors.
+        test_sql_error -> Test with SQL error.
+        test_io_error -> Test with IO error.
+        test_io_sql_errors -> Test with IO and SQL errors.
+        test_no_slave -> Test with no slave present.
+
+    """
+
+    def setUp(self):
+
+        """Function:  setUp
+
+        Description:  Initialization for unit testing.
+
+        Arguments:
+
+        """
+
+        self.master = Server()
+        self.slave = Server()
+        self.slaves = [self.slave]
+
+
+    def test_no_error(self):
+
+        """Function:  test_no_error
+
+        Description:  Test with no errors.
+
+        Arguments:
+
+        """
+
+        self.slave.io = None
+        self.slave.sql = None
+
+        self.assertFalse(mysql_clone.chk_slv_err(self.master, self.slaves))
+
+    def test_sql_error(self):
+
+        """Function:  test_sql_error
+
+        Description:  Test with SQL error.
+
+        Arguments:
+
+        """
+
+        self.slave.io = None
+
+        with gen_libs.no_std_out():
+            self.assertFalse(mysql_clone.chk_slv_err(self.master, self.slaves))
+
+    def test_io_error(self):
+
+        """Function:  test_io_error
+
+        Description:  Test with IO error.
+
+        Arguments:
+
+        """
+
+        self.slave.sql = None
+
+        with gen_libs.no_std_out():
+            self.assertFalse(mysql_clone.chk_slv_err(self.master, self.slaves))
+
+    def test_io_sql_errors(self):
+
+        """Function:  test_io_sql_errors
+
+        Description:  Test with IO and SQL errors.
+
+        Arguments:
+
+        """
+
+        with gen_libs.no_std_out():
+            self.assertFalse(mysql_clone.chk_slv_err(self.master, self.slaves))
+
+    def test_no_slave(self):
+
+        """Function:  test_no_slave
+
+        Description:  Test with no slave present.
+
+        Arguments:
+
+        """
+
+        with gen_libs.no_std_out():
+            self.assertFalse(mysql_clone.chk_slv_err(self.master, []))
+
+
+if __name__ == "__main__":
+    unittest.main()
