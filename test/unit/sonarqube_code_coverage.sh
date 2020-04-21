cf48af0b474ad5fb579b7915d55033ee4e812c4d
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-04-21 10:37:48 +0100

    Update test/unit/sonarqube_code_coverage.sh v2.1.0

--- test/unit/sonarqube_code_coverage.sh
+++ test/unit/sonarqube_code_coverage.sh
@@ -8,7 +8,8 @@ coverage erase
 
 echo ""
 echo "Running unit test modules in conjunction with coverage"
-coverage run -a --source=mysql_clone test/unit/clone/help_message.py
+coverage run -a --source=mysql_clone test/unit/mysql_clone/cfg_chk.py
+coverage run -a --source=mysql_clone test/unit/mysql_clone/help_message.py
 
 echo ""
 echo "Producing code coverage report"
